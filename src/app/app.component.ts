71b06d7fd46900514705c35b31210d077569f984
Damith jayawardhane <damithjayawardhane@gmail.com>
2021-09-18 22:27:20 +0530

    i18n file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'translateLanguage';
+
+  constructor (private translateService:TranslateService){}
+ 
+  public selectLanguage(event: any) {
+    this.translateService.use(event.target.value)
+  }
 }
